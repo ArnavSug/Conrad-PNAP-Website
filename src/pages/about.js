c1317f394cf3c0bc72cf7d2a4ccd81ebed2fd8cd
ArnavSug <55637569+ArnavSug@users.noreply.github.com>
2020-01-06 20:06:57 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import Layout from '../components/layout'
         return (
                 <Layout>
                 <h2>Here is a little info on us</h2>
-                <p>Our company (team) consists of 5 members: Arnav (TITLE), Aditya (TITLE), Nishvath (TITLE), Dhruv (TITLE), and Aman (TITLE). Our company started from a group of friends interested in the cyber-tech field with a willingness to take on the challenge of solving some of the mainstream technological problems in today’s society. Since we started, some of the biggest hurdles we’ve overcome was determining what our product would be and accomplish in general, how we would designate tasks to get all the tasks complete, and designing a fool-proof way to secure people’s health free from common allergens.</p>
+                <p>Our company (team) consists of 5 members: Arnav (CEO), Aditya (CTO), Nishvath (CFO), Dhruv (COO), and Aman (CIO). Our company started from a group of friends interested in the cyber-tech field with a willingness to take on the challenge of solving some of the mainstream technological problems in today’s society. Since we started, some of the biggest hurdles we’ve overcome was determining what our product would be and accomplish in general, how we would designate tasks to get all the tasks complete, and designing a fool-proof way to secure people’s health free from common allergens.</p>
                 <p><Link to="/contact">Find us here!</Link></p>
                </Layout> 
         )
